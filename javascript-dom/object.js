1d567bba75f6082ea222e26f276ba1c61fe0db36
NabilZulfalah <nabilzulfalah@gmail.com>
2024-01-30 11:00:33 +0700

    keren bingits

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -1,13 +1,42 @@
-var siswa =new Object();
-siswa.namadepan = "nabil";
-siswa.namabelakang = "zulfalah";
-siswa.alamat = "bandung";
+var siswa = new Object();
+siswa.namaDepan = "Nabil";
+siswa.namaBelakang = "Zulfalah";
+siswa.alamat = "Bandung";
 siswa.namalengkap = function() {
-    return this.namadepan + " " + this.namabelakang;
+    return this.namaDepan + " " + this.namaBelakang;
 };
 
-alert("Nama : " +siswa.namalengkap());
+//cara ke-2
+var siswa2 = {
+    namadepan : "Teti",
+    namabelakang : "Simatupang",
+    umur : "100",
+    namalengkap : function() {
+        return siswa2.namadepan + " " + siswa2.namabelakang;
+    }
+};
+
+alert("Nama kamu pasti " +siswa.namalengkap());
+alert("dan alamat kamu pasti di " + siswa.alamat + " yaa");
+alert("Nama kamu pasti " +siswa2.namalengkap());
+
 
 // function namalengkap () {
 
 // }
+
+//kalau manggil function harus pake () lagi, seperti function nya
+
+
+
+
+
+
+
+// var siswa =new Object();
+// siswa.namadepan = "nabil";
+// siswa.namabelakang = "zulfalah";
+// siswa.alamat = "bandung";
+// siswa.namalengkap = function() {
+//     return this.namadepan + " " + this.namabelakang;
+// };
--- latihan/latihan-b.js
+++ latihan/latihan-b.js
@@ -1,10 +1,20 @@
-//bikin var menu
+// Daftar menu dan harga
 var menu = {
-    "Nasi Goreng":15.000,
-    "Bebek Bakar":53.000,
-    "Es Teh":5.000,
-    "Nasi Rames":7.000
+    "Nasi telur Bledek": 15000,
+    "Mie Goreng": 10000,
+    "Mie Ayam": 25000,
+    "Sate bakar": 20000
 };
 
-//memilih menu
-var pesanan
+// Memilih menu
+var pesanan = prompt("Silakan pilih menu yang ingin dipesan:\n" + Object.keys(menu).join("\n"));
+var jumlah = prompt("Masukkan jumlah " + pesanan + " yang ingin dipesan:");
+
+// Menghitung total biaya
+var totalBiaya;
+if (menu.hasOwnProperty(pesanan)) {
+    totalBiaya = menu[pesanan] * parseInt(jumlah);
+    alert("Anda telah memesan " + jumlah + " " + pesanan + ". Total biaya: Rp" + totalBiaya);
+} else {
+    alert("Maaf, menu tidak tersedia.");
+}
